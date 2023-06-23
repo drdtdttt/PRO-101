c802e6559ab1b1a6899f98f988647b2486c4068f
drdtdttt <102718973+drdtdttt@users.noreply.github.com>
2023-06-23 19:28:36 +0530

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,25 @@
+import random 
+response=input('press y to roll')
+response='y'
+
+while response == 'y':
+    no = random.randint(1,6)
+
+    if(no==1):
+        print("the number on the dices 1")
+
+    if(no==2):
+        print("the number on the dices 2")
+
+    if(no==3):
+        print("the number on the dices 3")
+    
+    if(no==4):
+        print("the number on the dices 4")
+
+    if(no==5):
+        print("the number on the dices 5")
+
+    if(no==6):
+        print("the number on the dices 6")
+    
